095e969a4b7182abc0a8dff16ab4bb99b2402e34
Jack <git@jackhiggins.ie>
2019-01-29 21:23:55 +0100

    checkin python sample code

---
+++ python-listener.py
@@ -0,0 +1,19 @@
+import bluetooth
+
+bd_addr = "00:0D:19:03:AF:9F" //The address from the HC-05 sensor
+port = 1
+sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
+sock.connect((bd_addr,port))
+
+data = ""
+while 1:
+	try:
+		data += sock.recv(1024)
+		data_end = data.find('\n')
+		if data_end != -1:
+			rec = data[:data_end]
+			print data
+			data = data[data_end+1:]
+	except KeyboardInterrupt:
+		break
+sock.close()
