b90bd0e3aeded53343b3daabe0f33cfb37f2b70b
Jack Higgins <github@jackhiggins.ie>
2019-02-09 14:44:24 +0100

    add logging to bluetooth listener

--- python-listener.py
+++ python-listener.py
@@ -1,27 +1,33 @@
 import bluetooth
 import sys
 import socket
+import logging
 
 bd_addr = "00:0D:19:03:AF:9F" #Arduino bluetooth MAC
 port = 1
 
+logging.basicConfig(level=logging.DEBUG)
+
+logging.info("Starting up!")
+
 def wait_for_connection():
-	rcv_count = 0
+	rcv_count = -1
         
         sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
 	try:
-            print("DEBUG: Waiting for connection...")
+            logging.debug("Waiting for connection...")
             sock.connect((bd_addr, port))
+            rcv_count = 0
 
 	    while True:
-                print("DEBUG: Waiting for packet...")
+                logging.debug("Waiting for packet...")
 	    	data = sock.recv(1024)
-                print("DEBUG: Packet received.")
+                logging.debug("Packet received.")
                 rcv_count = rcv_count + 1
 
 	except bluetooth.btcommon.BluetoothError as e:
-            print("DEBUG:" + str(e))
-            print("DEBUG: Session finished.")
+            logging.debug(str(e))
+            logging.debug("Session finished.")
             sock.shutdown(socket.SHUT_RDWR)
             sock.close()
 
@@ -31,9 +37,9 @@ while True:
 	code = wait_for_connection()
         
         if code == 1:
-            print('INFO: Postman arrived!')
+            logging.info('Postman arrived!')
         elif code == 2:
-            print('INFO: Post read!')
+            logging.info('Post read!')
         else:
-            if code != 0: # Default behaviour
-                print('WARN: Unknown code: ' + str(code))
+            if code != -1:
+                logging.warn('Unknown code: ' + str(code))
